08b11fe7734071e9dd69760454167c2fb70a1352
gelinas <jlgelinas@gmail.com>
2019-08-19 17:57:59 -0400

    fished all three .js challenges, working on stretch

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -112,4 +112,16 @@ console.log(oldCars.length);
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
-console.log();
+for (let i = 0; i < inventory.length; i++) {
+  if (inventory[i].car_make ===  "BMW" | inventory[i].car_make === 'Audi') {BMWAndAudi.push(inventory[i]); }  
+}
+
+console.log(JSON.stringify(BMWAndAudi));
+
+//Stretch: creative a narrative output to Challenge 6
+var listBMWAndAudi = ""
+for (let i = 0; i < BMWAndAudi.length; i++) {
+  var listBMWAndAudi = listBMWAndAudi + BMWAndAudi[i].car_year + " " + BMWAndAudi[i].car_make + " " + BMWAndAudi[i].car_model + ", "
+}
+
+console.log(listBMWAndAudi);
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,32 @@
 // };
 // myFunction();
 
+let myfunction = () => "Function was invoked!"
+console.log(myfunction());
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => param;
+console.log(anotherFunction("Example"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => param1 + param2;
+console.log(add(1,2));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => param1 - param2;
+console.log(subtract(1,2));
 
 // Stretch
 
@@ -28,3 +39,14 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+// // function expression syntax
+// // const add = function(a,b) { 
+// //  return a + b;
+// // } 
+
+// // arrow function expression syntax
+// const add = (a,b) => { 
+//     return a + b;
+//    } 
+//    console.log(add(2,4)) // 6 
